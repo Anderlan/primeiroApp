a2b679dfbd16ae500e5e9e95895804035c43733a
anderlan <anderlansiqueira@gmail.com>
2023-07-25 11:13:24 -0300

    Atualizacao

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import Capa from './assets/capa.png';
+import Foto from './assets/foto.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ estilos.container }>
+      <Image style={ estilos.fotoTopo } source={Capa} />
+
+      <View style={ estilos.boxDescricao }>
+        <Image style={ estilos.fotoPerfil } source={Foto} />
+        <Text style={ estilos.titulo }>Insira seu nome aqui</Text>
+        <Text style={ estilos.cargo }>Insira um breve resumo sobre voce</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  fotoTopo: {
+    flex: 1,
+  },
+
+  boxDescricao: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  fotoPerfil: {
+    marginTop:20,
+    marginBottom:20,
+    width: 100,
+    height: 100,
+    borderRadius:50
+  },
+  titulo: {
+    marginBottom:10,
+    fontSize: 22,
+  },
+  cargo: {
+    textAlign: 'center',
+    marginHorizontal: 15,
+    fontSize: 16
+  }
 });
